79ee0e956ab0223dfcf2c1fb5de3e411a166ab6e
Ahbabuzzaman <160356472+ahbab-zaman@users.noreply.github.com>
2025-01-02 21:01:30 +0600

    cart delete api done

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 // const jwt = require("jsonwebtoken");
 // const cookieParser = require("cookie-parser");
 require("dotenv").config();
@@ -57,9 +57,18 @@ async function run() {
       res.send(result);
     });
 
-    app.get("/allCart", async (req, res) => {
-      const result = await cartsCollection.find().toArray();
+    app.get("/allCart/:email", async (req, res) => {
+      const email = req.params.email;
+      const query = { email: email };
+      const result = await cartsCollection.find(query).toArray();
       res.send(result);
+    });
+
+    app.delete("/cart/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await cartsCollection.deleteOne(query);
+      res.send(result)
     });
 
     console.log(
